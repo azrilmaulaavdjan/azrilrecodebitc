1872e5c17365b2e4de0d7735664459f824220ade
azrilmaulaavdjan <92779026+azrilmaulaavdjan@users.noreply.github.com>
2021-11-04 14:09:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,12 @@ exec(`cd /sdcard/download && play *mp3`)
 			ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 			}
 			let buff = await getBuffer(ppimg)
-			masuk =`Welcome To Group ${mdata.subject}\n\n*User* :@${num.split('@')[0]}\n*Time* :${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}\n\n𝙷𝚘𝚙𝚎 𝚢𝚘𝚞 𝚕𝚒𝚔𝚎 𝚒𝚝 𝚊𝚗𝚍 𝚍𝚘𝚗'𝚝 𝚏𝚘𝚛𝚐𝚎𝚝 𝚝𝚘 𝚛𝚎𝚊𝚍 𝚝𝚑𝚎 𝚐𝚛𝚘𝚞𝚙 𝚍𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗`
+			masuk =`┌❏ _Welcome To Group_
+│◦➛  {mdata.subject}
+├◦⎘ _Username_ : @${num.split('@')[0]}
+├◦⎘ _Time_ : ${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}
+├◦©® : _wa.me/6282147244748_
+└❏`
 			gbutsan = [{buttonId:'SERAH',buttonText:{displayText:'Welcome New Member'},type:1}]
 			mhan = await denz.prepareMessage(mdata.id, buff, MessageType.image, {thumbnail: buff})
 const buttonMessages = { imageMessage: mhan.message.imageMessage,
